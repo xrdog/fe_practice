61dc445bf34c90dc66450780344cb90c978c7fcc
罗龙光 <luolongguang@bytedance.com>
2021-05-12 20:53:38 +0800

    feat: 脚手架搭建工程完成

---
+++ main/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>11111</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
