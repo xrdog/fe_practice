4bdf5ce2015ccb0e2fd9abbe3ef61c542254b61e
罗龙光 <luolongguang@bytedance.com>
2021-05-19 18:35:07 +0800

    feat: 开发完成

--- main/src/pages/css-components/index.tsx
+++ main/src/pages/css-components/index.tsx
@@ -1,11 +1,31 @@
+import { Button } from "antd";
 import React, { FC } from "react";
 import Tick from "./tick/index";
 const CssComponents: FC = () => {
   console.log("this is CssComponents");
+  const onClick = () => {
+    const tick = (document.getElementById("tick")! as unknown) as SVGAElement;
+    tick.classList.remove("tick-animation");
+    tick.classList.add("tick-animation");
+    const circle = (document.getElementById(
+      "circle"
+    )! as unknown) as SVGAElement;
+    circle.classList.remove("circle-animation");
+    circle.classList.add("circle-animation");
+    const title = (document.getElementById("title")! as unknown) as SVGAElement;
+    title.classList.remove("title-animation");
+    title.classList.add("title-animation");
+  };
+
   return (
-    <>
+    <div className="page-box">
       <Tick></Tick>
-    </>
+      <span className="w-full inline align-center">
+        <Button type="primary" onClick={onClick}>
+          通过JS触发
+        </Button>
+      </span>
+    </div>
   );
 };
 
--- main/src/pages/css-components/tick/tick.tsx
+++ main/src/pages/css-components/tick/tick.tsx
@@ -1,15 +1,17 @@
 import React, { CSSProperties } from "react";
+import "./style/main.css";
 
 const flex: CSSProperties = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  minHeight: "100vh",
+  minHeight: "800px",
   flexDirection: "column",
 };
 
 export const tick = (
   <div style={flex}>
+    <input type="checkbox" className="mb16"></input>
     <svg width="400" height="400">
       <circle
         fill="none"
@@ -17,7 +19,11 @@ export const tick = (
         strokeWidth="20"
         cx="200"
         cy="200"
-        r="185"
+        r="190"
+        className="circle"
+        strokeLinecap="round"
+        transform="rotate(-90 200 200)"
+        id="circle"
       ></circle>
       <polyline
         fill="none"
@@ -26,8 +32,16 @@ export const tick = (
         points="88,214 173,284 304,138"
         strokeLinecap="round"
         strokeLinejoin="round"
+        className="tick"
+        id="tick"
       ></polyline>
     </svg>
-    <h1 style={{marginTop:'15px'}}> ~~~Success~~~</h1>
+    <h1
+      style={{ marginTop: "15px", fontSize: "40px" }}
+      id="title"
+      className="title"
+    >
+      ~~~Success~~~
+    </h1>
   </div>
 );
